29f652cf369c7c369238079ec3099b51f95c8032
GNTRSML <thedontcare@outlook.com>
2025-04-28 11:24:05 -0600

    Removed unnecessary lines

--- Arduino/4-20Analog_Testing.ino
+++ Arduino/4-20Analog_Testing.ino
@@ -2,8 +2,7 @@
 //Ensure there is 250 Ohm resistor between input and sensor return
 const int press = A5;
 int pressValue = 0;
-int pressMax = 1023;
-int pressMin = 0;
+
 void setup() {
   Serial.begin(9600);
   pinMode(press, INPUT);
@@ -11,8 +10,6 @@ void setup() {
 
 void loop() {
   pressValue = analogRead(press);
-  pressValue = constrain(pressValue, pressMin, pressMax);
-  pressValue = map(pressValue, 0, 1023, 0, 1023);
   Serial.println(pressValue);
   delay(500);
 }
